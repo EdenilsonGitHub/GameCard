a7ee91dd4b45d8d8f49fe147a6f423a56267e53b
NZBBR <natanzimmer@hotmail.com>
2021-05-23 12:01:25 -0300

    Controlador de aplicação

--- Assets/Scripts/ApplicationController.cs
+++ Assets/Scripts/ApplicationController.cs
@@ -19,4 +19,55 @@ public class ApplicationControler : MonoBehaviour
     public static void ExitGame(){
         Application.Quit ();
     }
+
+        //Sound Configurations//
+    //SFX//
+    public static void EnableSoundSFX(){
+        PlayerPrefs.SetInt ("SFXSound", 1);
+    }
+    
+    public static void DisableSoundSFX(){
+        PlayerPrefs.SetInt ("SFXSound", 0);
+    }
+
+    public static bool IsMuttedSoundSFX(){
+        if(PlayerPrefs.GetInt ("SFXSound") == 1)
+            return true;
+        
+        return false;
+    }
+    
+    public static void SetVolumeSFX(float volume){
+        PlayerPrefs.SetFloat ("SFXSoundVolume", volume);
+    }
+
+    public static float GetVolumeSFX(){
+        return PlayerPrefs.GetFloat ("SFXSoundVolume");
+    }
+
+
+    //Music
+    public static void EnableSoundMusic(){
+        PlayerPrefs.SetInt ("MusicSound", 1);
+    }
+    
+    public static void DisableSoundMusic(){
+        PlayerPrefs.SetInt ("MusicSound", 0);
+    }
+
+    public static bool IsMuttedSoundMusic(){
+         if(PlayerPrefs.GetInt ("MusicSound") == 1)
+            return true;
+        
+        return false;
+    }
+
+     public static void SetVolumeMusic(float volume){
+         PlayerPrefs.SetFloat ("MusicSoundVolume", volume);
+    }
+
+    public static float GetVolumeMusic(){
+        return PlayerPrefs.GetFloat ("MusicSoundVolume");
+    }
+        //Sound Configurations//
 }
---
+++ Assets/Scripts/OptionsMenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuController : MonoBehaviour
+{
+    public Toggle toggleSoundSFX;
+    public Toggle toggleSoundMusic;
+    public Slider sliderVolumeSFX;
+    public Slider sliderVolumeMusic;
+
+    void Start() {
+        toggleSoundSFX.isOn = ApplicationControler.IsMuttedSoundSFX ();
+        toggleSoundMusic.isOn = ApplicationControler.IsMuttedSoundMusic ();
+        sliderVolumeSFX.value = ApplicationControler.GetVolumeSFX ();
+        sliderVolumeMusic.value = ApplicationControler.GetVolumeMusic ();
+    }
+
+    void Update()
+    {
+        
+    }
+}
