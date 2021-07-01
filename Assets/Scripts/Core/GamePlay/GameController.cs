f1ca1d83547aced9269931e4446d4cce43639aad
Edenilson <56543408+EdenilsonGitHub@users.noreply.github.com>
2021-06-30 22:45:42 -0300

    Criando o menu dentro do gameplay

---
+++ Assets/Scripts/Core/GamePlay/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    public int totalMana;
+    public PlayerController player1;
+
+    public int currentTurn = 1;
+
+    public static GameController instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ExitGame() {
+        Application.LoadLevel ("Menu");
+    }
+}
