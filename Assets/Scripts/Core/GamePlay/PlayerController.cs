576f050043be43a333643b8208319b5ba5b3c37a
Edenilson <56543408+EdenilsonGitHub@users.noreply.github.com>
2021-07-02 22:08:29 -0300

    Criando o tabuleiro do jogo

--- Assets/Scripts/Core/GamePlay/PlayerController.cs
+++ Assets/Scripts/Core/GamePlay/PlayerController.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : LifeBase
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start ();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        base.Update ();
+    }
+
+    public override void OnDie()
+    {
+
+    }
+
+    public override void OnDamage()
+    {
+
     }
 }
---
+++ Assets/Scripts/Core/LifeBase.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class LifeBase : MonoBehaviour
+{
+
+    public int totalLife;
+
+    private int currentLife;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        currentLife = totalLife;
+    }
+    
+    // Update is called once per frame
+    protected void Update()
+    {
+        
+    }
+
+    public void ApplyDamage(int damage) {
+        currentLife -= damage;
+        OnDamage ();
+
+        if (currentLife <= 0) {
+            OnDie();
+        }
+    }
+
+    public abstract void OnDamage();
+    public abstract void OnDie();
+
+}
