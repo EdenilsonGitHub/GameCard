07cf433b431b6a1ae6090956ca5263df9724267e
Edenilson <56543408+EdenilsonGitHub@users.noreply.github.com>
2021-07-09 16:09:47 -0300

    Ajustando o prefab da carta parte 2

--- Assets/Scripts/Core/Cards/CardBase.cs
+++ Assets/Scripts/Core/Cards/CardBase.cs
@@ -6,9 +6,15 @@ public class CardBase : LifeBase
 {
 
     public int totalManaRequest;
+    public string nameCard;
+    public string descriptionCard;
+    public Texture imgCard;
 
     public TextMesh textLife;
     public TextMesh textMana;
+    public TextMesh textNameCard;
+    public TextMesh textDescriptionCard;
+    public Renderer renderImgCard;
 
     // Start is called before the first frame update
     protected void Start()
@@ -16,6 +22,9 @@ public class CardBase : LifeBase
         base.Start();
         textLife.text = currentLife.ToString();
         textMana.text = totalManaRequest.ToString();
+        textNameCard.text = nameCard;
+        textDescriptionCard.text = descriptionCard;
+        renderImgCard.material.mainTexture = imgCard;
     }
 
     // Update is called once per frame
