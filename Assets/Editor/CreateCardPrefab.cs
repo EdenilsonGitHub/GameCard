6b3e67f11594bb0944af1003a7021c8dbafd0ae3
NZBBR <natanzimmer@hotmail.com>
2021-07-10 16:28:35 -0300

    Inserido o Deck

--- Assets/Editor/CreateCardPrefab.cs
+++ Assets/Editor/CreateCardPrefab.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 class CreatePrefabFromSelected
 {
 	const string menuTitle = "Cards/Create Card From Selected";
+	const string menuTitle2 = "Cards/Update Cards";
  
 	/// <summary>
 	/// Creates a prefab from the selected game object.
 	/// </summary>
 	[MenuItem (menuTitle)]
-	static void CreatePrefab ()
+	static void CreatePrefab (GameObject o = null)
 	{
-		GameObject obj = Selection.activeGameObject;
+		GameObject obj;
+		if (o == null)
+			obj = Selection.activeGameObject;
+		else
+			obj = o;
+		
 		string name = obj.name;
  
 		Object prefab = EditorUtility.CreateEmptyPrefab("Assets/Prefabs/Cards/Monsters/" + name + ".prefab");
 		EditorUtility.ReplacePrefab(obj, prefab);
 		AssetDatabase.Refresh();
+	}
+
+	[MenuItem (menuTitle2)]
+	static void UpdatePrefabs ()
+	{
+		GameObject[] objs = Selection.gameObjects;
+
+		foreach (GameObject o in objs){
+			CreatePrefab(o);
+		}
+		
 	}
  
 	/// <summary>
@@ -25,6 +42,12 @@ class CreatePrefabFromSelected
 	/// <remarks>The item will be disabled if no game object is selected.</remarks>
 	[MenuItem (menuTitle, true)]
 	static bool ValidateCreatePrefab ()
+	{
+		return Selection.activeGameObject != null;
+	}
+
+	[MenuItem (menuTitle2, true)]
+	static bool ValidateUpdateCreatePrefab ()
 	{
 		return Selection.activeGameObject != null;
 	}
---
+++ Assets/Scripts/Core/GamePlay/DeckController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckController : MonoBehaviour
+{
+    private Vector3 initialSide;
+    private int totalInitialCards;
+    public List<CardBase> initListCards;
+
+    // Start is called before the first frame update
+    void Start ()
+    {
+        initialSide = transform.localScale;
+        totalInitialCards = initListCards.Count;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+      if (Input.GetKeyDown(KeyCode.P))   
+        GetCard();
+    }
+
+    public void GetCard(){
+        if(initListCards.Count > 0) {
+        int randCardIndex = Random.Range(0,initListCards.Count);
+        CardBase selectedCard = initListCards [randCardIndex];
+        initListCards.RemoveAt(randCardIndex);
+
+        Instantiate (selectedCard.gameObject, Vector3.zero, selectedCard.transform.rotation);
+        ResizeDeck ();
+    }
+}
+
+    private void ResizeDeck(){
+     
+        Vector3 newSize = transform.localScale;
+     
+       newSize.y = initListCards.Count * initialSide.y / totalInitialCards;
+       transform.localScale = newSize;
+
+       if (initListCards.Count == 0){
+
+           GetComponent<Renderer>().enabled = false;
+       }
+    }
+
+}
